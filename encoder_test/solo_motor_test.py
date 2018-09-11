1809f4ce151bd12dee2c4c9d1b6be82d61f26554
yanstolyarov <yanstolyarov@mail.ru>
2018-09-11 16:42:09 +0300

    a

--- encoder_test/solo_motor_test.py
+++ encoder_test/solo_motor_test.py
@@ -14,23 +14,25 @@ GPIO.setup(m2,GPIO.OUT)
 GPIO.setup(enb,GPIO.OUT)
 
 #To create a PWM instance: p = GPIO.PWM(channel, frequency)
-#p_w = GPIO.PWM(enb,100)
+p_w = GPIO.PWM(enb,100)
 
-#p_w.start(0)
+p_w.start(0)
 
 
 def motor(x):
     if x > 0:
-        #p_w.ChangeDutyCycle(x)
-        GPIO.output(enb, GPIO.HIGH)
+        p_w.ChangeDutyCycle(x)
+        #GPIO.output(enb, GPIO.HIGH)
         GPIO.output(e2, GPIO.HIGH)
         GPIO.output(m2, GPIO.LOW)
     if x < 0:
-        #p_w.ChangeDutyCycle(-x)
-        GPIO.output(enb, GPIO.HIGH)
+        p_w.ChangeDutyCycle(x)
+        #GPIO.output(enb, GPIO.HIGH)
         GPIO.output(m2, GPIO.HIGH)
         GPIO.output(e2, GPIO.LOW)
 
 while 1:
     motor(90)
     time.sleep(3)
+    motor(0)
+    time.sleep(1)
